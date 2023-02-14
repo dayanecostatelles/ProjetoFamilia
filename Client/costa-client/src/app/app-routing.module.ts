b02cdc16011fa122c7431ce06eaac5eb1c71e29c
dayanecostatelles <dayanecostatelles@outlook.com>
2023-02-14 19:02:05 -0300

    corigido a tela de editar

--- Client/costa-client/src/app/app-routing.module.ts
+++ Client/costa-client/src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
         component: AdicionarClienteComponent,
       },
       {
-        path: 'editar',
+        path: 'editar/:cpf',
         component: EditarClienteComponent,
       },
       {
