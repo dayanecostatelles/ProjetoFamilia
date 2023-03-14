0cf3a42f239ba9c864675f755213fa727e8f13b1
dayanecostatelles <dayanecostatelles@outlook.com>
2023-03-14 20:36:02 -0300

    FOI IMPLEMENTADO O front end do servico

---
+++ Client/costa-client/src/app/feature/servico/buscar-servico/buscar-servico.component.ts
@@ -0,0 +1,49 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { take } from 'rxjs';
+import { IServico } from '../servico.model';
+import { ServicoService } from '../servico.service.component';
+
+
+@Component({
+  selector: 'app-buscar-servico',
+  templateUrl: './buscar-servico.component.html',
+  styleUrls: ['./buscar-servico.component.css']
+})
+export class BuscarServicoComponent implements OnInit {
+
+    @Input() listaServico?: IServico[];
+  
+    servicoBuscado:IServico = {} as IServico;
+    idServico: string ='';
+    dadosEncontrados?:boolean;
+ 
+
+  constructor(private service: ServicoService) { }
+
+  ngOnInit(): void {
+  }
+
+  buscarServico() : void {
+    this.dadosEncontrados = false;
+   
+      this.service.buscarServico(this.idServico)
+      .pipe(take(1))
+      .subscribe((dados) => {
+        if(dados){
+          this.servicoBuscado = dados;
+          debugger;
+          this.dadosEncontrados = true;
+
+        }        
+      });
+    
+  }
+  
+
+  
+
+    
+
+}
